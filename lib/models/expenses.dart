bfa757da8281b4139c0ab697e1f31d970596cbba
AswinAzikar <azwinaswin8@gmail.com>
2024-06-22 05:56:35 +0530

    1st commit

---
+++ lib/models/expenses.dart
@@ -0,0 +1,39 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/models/expenses_list.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExprenses = [
+    Expense(
+        date: DateTime.now(),
+        title: "Course",
+        amount: 18.4,
+        category: Category.travel),
+    Expense(
+        date: DateTime.now(),
+        title: "Cinema",
+        amount: 15.7,
+        category: Category.travel),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text("the chart"),
+          Expanded(
+            child: ExpesnsesList(expenses: _registeredExprenses),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/models/expenses_list.dart
@@ -0,0 +1,17 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/models/expenses.dart';
+import 'package:flutter/material.dart';
+
+class ExpesnsesList extends StatelessWidget {
+  const ExpesnsesList({super.key, required this.expenses});
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Text(expenses[index].title),
+    );
+  }
+}
