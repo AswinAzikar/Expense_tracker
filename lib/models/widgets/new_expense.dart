42c16f21da441ac801f718147c00cee1082bca44
AswinAzikar <azwinaswin8@gmail.com>
2024-06-24 09:29:00 +0530

    text field for amount , added cancel button

--- lib/models/widgets/new_expense.dart
+++ lib/models/widgets/new_expense.dart
@@ -10,55 +10,108 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(
-              controller: _titleController,
-              maxLength: 50,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: const BorderSide(
-                    color: AppColors.green,
-                    width: 1.0,
-                  ),
+  Widget build(BuildContext context) {
+    double screenheight = MediaQuery.sizeOf(context).height;
+    double screenwidth = MediaQuery.sizeOf(context).width;
+
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: const BorderSide(
+                  color: AppColors.green,
+                  width: 1.0,
                 ),
-                label: const Text(
-                  "Title",
-                  style: TextStyle(color: Colors.white),
-                ),
-                enabledBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: AppColors.green,
-                    width: 1.0,
-                  ),
+              ),
+              label: const Text(
+                "Title",
+                style: TextStyle(color: Colors.white),
+              ),
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: AppColors.green,
+                  width: 1.0,
                 ),
               ),
             ),
-            Row(
-              children: [
-                ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStateProperty.all(AppColors.green),
-                      foregroundColor: WidgetStateProperty.all(Colors.black),
-                    ),
-                    onPressed: () {
-                      print(_titleController.text);
-                    },
-                    child: const Text("save"))
-              ],
-            )
-          ],
-        ),
-      );
+          ),
+          TextField(
+            controller: _amountController,
+            maxLength: 5,
+            keyboardType: TextInputType.numberWithOptions(decimal: true),
+            decoration: InputDecoration(
+              prefixText: '₹',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: const BorderSide(
+                  color: AppColors.green,
+                  width: 1.0,
+                ),
+              ),
+              label: const Text(
+                "Amount",
+                style: TextStyle(color: Colors.white),
+              ),
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: AppColors.green,
+                  width: 1.0,
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(AppColors.green),
+                  foregroundColor: WidgetStateProperty.all(Colors.black),
+                ),
+                onPressed: () {
+                  print(_titleController.text);
+                },
+                child: const Text(
+                  "save",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                width: 0.02 * screenwidth,
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(
+                      const Color.fromARGB(255, 44, 43, 42)),
+                  foregroundColor: WidgetStateProperty.all(Colors.red),
+                ),
+                onPressed: () {
+                  print(_titleController.text);
+                },
+                child: const Text(
+                  "cancel",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
